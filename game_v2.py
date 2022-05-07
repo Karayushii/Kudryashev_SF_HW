4b2c35f22d3456c86164fc63c559e1cd0b8005e2
Oleg <olkud106@gmail.com>
2022-05-07 23:13:14 +0300

    new

--- game_v2.py
+++ game_v2.py
@@ -15,17 +15,22 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-    first_num = 1
-    last_num = 101
-
+    first_num = 1  # первое число в интервале выбора компьютера
+    last_num = 101  # последнее число в интервале выбора компьютера
     while True:
         predict_number = np.random.randint(first_num, last_num)
         # предполагаемое число
         count += 1
         if number < predict_number:
+            # если загаданное число меньше выбранного, область поиска
+            # сокращается по верхнему пределу до выбраноого числа(не
+            # включительно)
             last_num = predict_number
             continue
         if number > predict_number:
+            # если загаданное число больше выбранного, область поиска
+            # сокращается по нижнему пределу до выбраноого числа
+            # (включительно)
             first_num = predict_number
             continue
         if number == predict_number:
